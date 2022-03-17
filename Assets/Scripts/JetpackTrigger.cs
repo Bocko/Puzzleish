b73021d4a231f4a8a8a16d11ffdbca1c3678c78b
Gellért Botond László <laszlogb@edu.bme.hu>
2022-03-17 10:03:03 +0100

    added triggers to turn the jet pack on and off added prefab of the jetpack trigger

---
+++ Assets/Scripts/JetpackTrigger.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetpackTrigger : MonoBehaviour
+{
+    public enum JetpackStatus { ON, OFF }
+
+    public JetpackStatus statusToSetJetpack;
+
+    void OnTriggerEnter(Collider other)
+    {
+        PlayerJetpack pjp = other.GetComponent<PlayerJetpack>();
+        if (pjp != null)
+        {
+            pjp.isOn = statusToSetJetpack == JetpackStatus.ON;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, transform.localScale);
+        Gizmos.DrawIcon(transform.position, "d_Spotlight Icon", false, statusToSetJetpack == JetpackStatus.ON ? Color.green : Color.red);
+    }
+}
