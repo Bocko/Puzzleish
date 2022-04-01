407ece959fe6341941d15250017b112eb2ff7d66
Gellért Botond László <laszlogb@edu.bme.hu>
2022-04-01 10:40:14 +0200

    added collision check to laser added blood effect when laser hits player added door at the and of area 2

--- Assets/Scripts/Puzzle/LaserMover.cs
+++ Assets/Scripts/Puzzle/LaserMover.cs
@@ -6,12 +6,18 @@ public class LaserMover : MonoBehaviour
 {
     public float offset = 17.2f;
     public float laserTime = 5;
+    public Transform rayOriginAndDirection;
+    public Transform respawnPoint;
+    public LayerMask laserMask;
+    public float laserDistance;
+    public ParticleSystem redJuice;
+
     void Start()
     {
         StartCoroutine(LaserMoving());
     }
 
-    IEnumerator LaserMoving()
+    IEnumerator LaserMoving()//add raycast for collision check
     {
         float percent = 0;
         float laserSpeed = 1 / laserTime;
@@ -19,10 +25,19 @@ public class LaserMover : MonoBehaviour
         {
             percent += Time.deltaTime * laserSpeed;
             float z = Mathf.PingPong(percent, offset * 2) - offset;
-            print(z);
+            CheckLaserCollision();
             transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, z);
 
             yield return null;
+        }
+    }
+
+    void CheckLaserCollision()
+    {
+        if(Physics.Raycast(rayOriginAndDirection.position,rayOriginAndDirection.forward,out RaycastHit hitinfo, laserDistance, laserMask))
+        {
+            Instantiate(redJuice, hitinfo.point,Quaternion.identity);
+            hitinfo.transform.position = respawnPoint.position;
         }
     }
 }
