71829ba8e869d515462510bf2c26c6820cfdc1d2
Gellért Botond László <laszlogb@edu.bme.hu>
2022-04-07 15:17:36 +0200

    reworked door Handler to not stutter when its being closed and opend quickly

--- Assets/Scripts/Puzzle/DoorHandler.cs
+++ Assets/Scripts/Puzzle/DoorHandler.cs
@@ -12,11 +12,14 @@ public class DoorHandler : MonoBehaviour
     public facingDirection facing;
     public float openingTime = 0.5f;
     public state startingState;
+    [Range(0, 1)]
+    public float closePercent = 1;
 
     state currentState;
     float dirSide;
     float dirScale;
     Vector3 originalPos;
+    bool isSomethingRunning;
 
     void Start()
     {
@@ -39,6 +42,40 @@ public class DoorHandler : MonoBehaviour
         if (stateToMove != currentState)
         {
             currentState = stateToMove;
+
+            if (isSomethingRunning) return;
+
+            switch (direction)//calling the correct method to get the proper opening direction
+            {
+                case openingDirection.UP:
+                    StartCoroutine(Vertical2(1));
+                    break;
+                case openingDirection.DOWN:
+                    StartCoroutine(Vertical2(-1));
+                    break;
+                case openingDirection.LEFT:
+                    if (facing == facingDirection.X)
+                    {
+                        StartCoroutine(HorizontalFaceX(-1));
+                    }
+                    else
+                    {
+                        StartCoroutine(HorizontalFaceZ(-1));
+                    }
+                    break;
+                case openingDirection.RIGHT:
+                    if (facing == facingDirection.X)
+                    {
+                        StartCoroutine(HorizontalFaceX(1));
+                    }
+                    else
+                    {
+                        StartCoroutine(HorizontalFaceZ(1));
+                    }
+                    break;
+            }
+
+            /*
             switch (direction)//calling the correct method to get the proper opening direction
             {
                 case openingDirection.UP:
@@ -58,9 +95,62 @@ public class DoorHandler : MonoBehaviour
                     StartCoroutine(Horizontal(1));
                     break;
             }
+            */
         }
     }
 
+    IEnumerator Vertical2(int openingDir)//1 UP, -1 DOWN
+    {
+        isSomethingRunning = true;
+        float openingSpeed = 1 / openingTime;
+
+        while ((currentState == state.OPEN && closePercent > 0) || (currentState == state.CLOSED && closePercent < 1))
+        {
+            closePercent += Time.deltaTime * openingSpeed * (currentState == state.OPEN ? -1 : 1);
+
+            transform.localScale = new Vector3(transform.localScale.x, Mathf.Lerp(0, dirScale, closePercent), transform.localScale.z);
+            transform.position = new Vector3(transform.position.x, Mathf.Lerp(originalPos.y + dirSide * openingDir, originalPos.y, closePercent), transform.position.z);
+
+            yield return null;
+        }
+
+        isSomethingRunning = false;
+    }
+
+    IEnumerator HorizontalFaceX(int openingDir)//1 RIGHT, -1 LEFT
+    {
+        isSomethingRunning = true;
+        float openingSpeed = 1 / openingTime;
+
+        while ((currentState == state.OPEN && closePercent > 0) || (currentState == state.CLOSED && closePercent < 1))
+        {
+            closePercent += Time.deltaTime * openingSpeed * (currentState == state.OPEN ? -1 : 1);
+
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, Mathf.Lerp(0, dirScale, closePercent));
+            transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Lerp(originalPos.z + dirSide * openingDir * -1, originalPos.z, closePercent));
+
+            yield return null;
+        }
+        isSomethingRunning = false;
+    }
+
+    IEnumerator HorizontalFaceZ(int openingDir)//1 RIGHT, -1 LEFT
+    {
+        isSomethingRunning = true;
+        float openingSpeed = 1 / openingTime;
+
+        while ((currentState == state.OPEN && closePercent > 0) || (currentState == state.CLOSED && closePercent < 1))
+        {
+            closePercent += Time.deltaTime * openingSpeed * (currentState == state.OPEN ? -1 : 1);
+
+            transform.localScale = new Vector3(Mathf.Lerp(0, dirScale, closePercent), transform.localScale.y, transform.localScale.z);
+            transform.position = new Vector3(Mathf.Lerp(originalPos.x + dirSide * openingDir, originalPos.x, closePercent), transform.position.y, transform.position.z);
+
+            yield return null;
+        }
+        isSomethingRunning = false;
+    }
+    /*
     IEnumerator Vertical(int openingDir)//1 UP, -1 DOWN
     {
         float percent = 0;
@@ -100,4 +190,5 @@ public class DoorHandler : MonoBehaviour
             yield return null;
         }
     }
+    */
 }
