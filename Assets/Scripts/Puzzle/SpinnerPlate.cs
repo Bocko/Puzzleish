dbe596d337e73699ea6c34899938fe3e753e32d2
Gellért Botond László <laszlogb@edu.bme.hu>
2022-05-24 00:31:31 +0200

    removed while true from spinnerplate

--- Assets/Scripts/Puzzle/SpinnerPlate.cs
+++ Assets/Scripts/Puzzle/SpinnerPlate.cs
@@ -7,14 +7,23 @@ public class SpinnerPlate : MonoBehaviour
 
     public float degreePerSec = 90;
     public RotateDirection dir = RotateDirection.CW;
-    void Start()
+
+    private bool spinnerEnabled;
+
+    private void OnEnable()
     {
+        spinnerEnabled = true;
         StartCoroutine(Spin());
+    }
+
+    private void OnDisable()
+    {
+        spinnerEnabled = false;
     }
 
     IEnumerator Spin()
     {
-        while (true)
+        while (spinnerEnabled)
         {
             transform.rotation *= Quaternion.Euler((dir == RotateDirection.CW ? 1 : -1) * degreePerSec * Time.deltaTime * Vector3.up);
 
