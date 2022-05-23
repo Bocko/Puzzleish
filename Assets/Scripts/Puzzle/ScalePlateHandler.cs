fdf7592ed91ffeb48dc45c61d0da742e6f58f062
Gellért Botond László <laszlogb@edu.bme.hu>
2022-05-24 00:27:28 +0200

    added variable to hold parents transform removed hardcoded tags

--- Assets/Scripts/Puzzle/ScalePlateHandler.cs
+++ Assets/Scripts/Puzzle/ScalePlateHandler.cs
@@ -5,10 +5,11 @@ public class ScalePlateHandler : MonoBehaviour
 {
     public event System.Action<float> WeightChange;
     List<Collider> weights = new List<Collider>();
+    private readonly string scaleTag = "ScaleWeight";
 
     void OnTriggerEnter(Collider other)//sending event when a new cube is inside the plate trigger and adding it to a list
     {
-        if (other.CompareTag("ScaleWeight"))
+        if (other.CompareTag(scaleTag))
         {
             weights.Add(other);
             WeightChange?.Invoke(other.GetComponent<ScaleWeight>().weight);
@@ -17,7 +18,7 @@ public class ScalePlateHandler : MonoBehaviour
 
     void OnTriggerExit(Collider other)//sending event when a new cube is inside the plate trigger and removing it form the list
     {
-        if (other.CompareTag("ScaleWeight"))
+        if (other.CompareTag(scaleTag))
         {
             weights.Remove(other);
             WeightChange?.Invoke(-other.GetComponent<ScaleWeight>().weight);
@@ -33,7 +34,8 @@ public class ScalePlateHandler : MonoBehaviour
     {
         foreach (Collider weight in weights)
         {
-            weight.transform.parent.transform.position = new Vector3(weight.transform.parent.transform.position.x, newPos, weight.transform.parent.transform.position.z);
+            Transform weightT = weight.transform.parent.transform;
+            weightT.position = new Vector3(weightT.position.x, newPos, weightT.position.z);
         }
     }
 }
