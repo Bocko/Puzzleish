cf361c8bf2c7ad2213ceeca07aa1194ed2bf96da
Gellért Botond László <laszlogb@edu.bme.hu>
2022-05-24 09:38:17 +0200

    separeted properties in CnETrigger removed hardcoded messages made waitForSeconds into variables

--- Assets/Scripts/Triggers/CauseAndEffectTrigger.cs
+++ Assets/Scripts/Triggers/CauseAndEffectTrigger.cs
@@ -3,21 +3,34 @@ using UnityEngine;
 
 public class CauseAndEffectTrigger : MonoBehaviour
 {
-    bool played;
+    [Header("Settings")]
     public float teleportFadeTime = 0.1f;
     public float waitTimeAfterTeleport = 1f;
     public float firstMessageWaitTime = 2f;
     public float waitTimeAfterFirstMessage = 2f;
     public float secondMessageWaitTime = 3f;
 
+    [Header("Tag")]
+    public string playerTag = "Player";
+
+    [Header("Texts")]
+    public string firstMessageText = "What the hell?!..";
+    public string secondMessageText = "Its like I have traveled forward in time.";
+
+    WaitForSeconds waitAfterTeleport;
+    WaitForSeconds waitAfterMessage;
+    bool played;
+
     void Start()
     {
         played = false;
+        waitAfterTeleport = new WaitForSeconds(waitTimeAfterTeleport);
+        waitAfterMessage = new WaitForSeconds(waitTimeAfterFirstMessage);
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag(playerTag))
         {
             if (!played)
             {
@@ -32,22 +45,22 @@ public class CauseAndEffectTrigger : MonoBehaviour
 
         yield return playerCnE.ExternalTeleport(teleportFadeTime);
 
-        yield return new WaitForSeconds(waitTimeAfterTeleport);
+        yield return waitAfterTeleport;
 
         if (NotificationManager.instance != null)
         {
-            yield return NotificationManager.instance.ShowNotificationSync("What the hell?!..", firstMessageWaitTime);
+            yield return NotificationManager.instance.ShowNotificationSync(firstMessageText, firstMessageWaitTime);
         }
 
-        yield return new WaitForSeconds(waitTimeAfterFirstMessage);
+        yield return waitAfterMessage;
 
         yield return playerCnE.ExternalTeleport(teleportFadeTime);
 
-        yield return new WaitForSeconds(waitTimeAfterTeleport);
+        yield return waitAfterTeleport;
 
         if (NotificationManager.instance != null)
         {
-            yield return NotificationManager.instance.ShowNotificationSync("Its like I have traveled forward in time.", secondMessageWaitTime);
+            yield return NotificationManager.instance.ShowNotificationSync(secondMessageText, secondMessageWaitTime);
         }
     }
 
