aa9b382343745a5b4093a13ee491f92b0c6926e4
Gellért Botond László <laszlogb@edu.bme.hu>
2022-05-24 16:45:11 +0200

    reworked doorHandler to use only one while cycle

--- Assets/Scripts/Puzzle/DoorHandler.cs
+++ Assets/Scripts/Puzzle/DoorHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -19,6 +20,8 @@ public class DoorHandler : MonoBehaviour
     float dirScale;
     Vector3 originalPos;
     bool isSomethingRunning;
+    int openingDir;
+    Action correctDirAndFace;
 
     void Start()
     {
@@ -47,36 +50,60 @@ public class DoorHandler : MonoBehaviour
             switch (direction)//calling the correct method to get the proper opening direction
             {
                 case openingDirection.UP:
-                    StartCoroutine(Vertical2(1));
+                    openingDir = 1;
+                    correctDirAndFace = Vertical;
                     break;
                 case openingDirection.DOWN:
-                    StartCoroutine(Vertical2(-1));
+                    openingDir = -1;
+                    correctDirAndFace = Vertical;
                     break;
                 case openingDirection.LEFT:
+                    openingDir = -1;
                     if (facing == facingDirection.X)
                     {
-                        StartCoroutine(HorizontalFaceX(-1));
+                        correctDirAndFace = HorizFaceX;
                     }
                     else
                     {
-                        StartCoroutine(HorizontalFaceZ(-1));
+                        correctDirAndFace = HorizFaceZ;
                     }
                     break;
                 case openingDirection.RIGHT:
+                    openingDir = 1;
+
                     if (facing == facingDirection.X)
                     {
-                        StartCoroutine(HorizontalFaceX(1));
+                        correctDirAndFace = HorizFaceX;
                     }
                     else
                     {
-                        StartCoroutine(HorizontalFaceZ(1));
+                        correctDirAndFace = HorizFaceZ;
                     }
                     break;
             }
+            StartCoroutine(DoorMover());
         }
     }
 
-    IEnumerator Vertical2(int openingDir)//1 UP, -1 DOWN
+    void Vertical()
+    {
+        transform.localScale = new Vector3(transform.localScale.x, Mathf.Lerp(0, dirScale, closePercent), transform.localScale.z);
+        transform.position = new Vector3(transform.position.x, Mathf.Lerp(originalPos.y + dirSide * openingDir, originalPos.y, closePercent), transform.position.z);
+    }
+
+    void HorizFaceX()
+    {
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, Mathf.Lerp(0, dirScale, closePercent));
+        transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Lerp(originalPos.z + dirSide * openingDir * -1, originalPos.z, closePercent));
+    }
+
+    void HorizFaceZ()
+    {
+        transform.localScale = new Vector3(Mathf.Lerp(0, dirScale, closePercent), transform.localScale.y, transform.localScale.z);
+        transform.position = new Vector3(Mathf.Lerp(originalPos.x + dirSide * openingDir, originalPos.x, closePercent), transform.position.y, transform.position.z);
+    }
+
+    IEnumerator DoorMover()
     {
         isSomethingRunning = true;
         float openingSpeed = 1 / openingTime;
@@ -85,46 +112,11 @@ public class DoorHandler : MonoBehaviour
         {
             closePercent += Time.deltaTime * openingSpeed * (currentState == state.OPEN ? -1 : 1);
 
-            transform.localScale = new Vector3(transform.localScale.x, Mathf.Lerp(0, dirScale, closePercent), transform.localScale.z);
-            transform.position = new Vector3(transform.position.x, Mathf.Lerp(originalPos.y + dirSide * openingDir, originalPos.y, closePercent), transform.position.z);
+            correctDirAndFace();
 
             yield return null;
         }
 
-        isSomethingRunning = false;
-    }
-
-    IEnumerator HorizontalFaceX(int openingDir)//1 RIGHT, -1 LEFT
-    {
-        isSomethingRunning = true;
-        float openingSpeed = 1 / openingTime;
-
-        while ((currentState == state.OPEN && closePercent > 0) || (currentState == state.CLOSED && closePercent < 1))
-        {
-            closePercent += Time.deltaTime * openingSpeed * (currentState == state.OPEN ? -1 : 1);
-
-            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, Mathf.Lerp(0, dirScale, closePercent));
-            transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Lerp(originalPos.z + dirSide * openingDir * -1, originalPos.z, closePercent));
-
-            yield return null;
-        }
-        isSomethingRunning = false;
-    }
-
-    IEnumerator HorizontalFaceZ(int openingDir)//1 RIGHT, -1 LEFT
-    {
-        isSomethingRunning = true;
-        float openingSpeed = 1 / openingTime;
-
-        while ((currentState == state.OPEN && closePercent > 0) || (currentState == state.CLOSED && closePercent < 1))
-        {
-            closePercent += Time.deltaTime * openingSpeed * (currentState == state.OPEN ? -1 : 1);
-
-            transform.localScale = new Vector3(Mathf.Lerp(0, dirScale, closePercent), transform.localScale.y, transform.localScale.z);
-            transform.position = new Vector3(Mathf.Lerp(originalPos.x + dirSide * openingDir, originalPos.x, closePercent), transform.position.y, transform.position.z);
-
-            yield return null;
-        }
         isSomethingRunning = false;
     }
 }
